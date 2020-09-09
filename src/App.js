c4bc6b82f02f7dff400d94a0e2f4783cd6f88925
tarmes <traarmes@gmail.com>
2020-09-09 19:34:48 -0400

    axios request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
 import { API_KEY, BASE_URL } from './constants'
 
 function App() {
+
+  const [photo, setPhoto] = useState({});
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(err => {
+        debugger
+      })
+  }, [])
+
+
   return (
     <div className="App">
       <p>
